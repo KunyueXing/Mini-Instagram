7c4c2e814f5880a1271c55d6d4c91591cbedb857
Kunyue Xing <scarlettmi@Kunyues-Mac-mini.local>
2023-05-13 23:36:28 -0700

    New post page: add close page function, user then go to homepage.

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/UI_Activity/NewPostActivity.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/UI_Activity/NewPostActivity.java
@@ -2,15 +2,42 @@ package com.example.miniinstagram.UI_Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.miniinstagram.R;
 
+import org.w3c.dom.Text;
+
 public class NewPostActivity extends AppCompatActivity {
+
+    private ImageView close;
+    private ImageView addImageView;
+    private TextView postTextView;
+    private TextView postContentTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        close = findViewById(R.id.closeImageView);
+        addImageView = findViewById(R.id.addImageImageView);
+        postTextView = findViewById(R.id.postTextView);
+        postContentTextView = findViewById(R.id.postContentTextView);
+
+        //when user click on close, will go back to homepage
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewPostActivity.this, HomepageActivity.class));
+                finish();
+            }
+        });
+
     }
 }
