a63c568901b154bde447caa0ecbc0e3a4f5c83c5
Kunyue Xing <scarlettyue1991@gmail.com>
2023-12-04 15:21:20 -0800

    Started to work on mypage fragment and attached it with the interface

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/MypageFragment.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/MypageFragment.java
@@ -1,22 +1,103 @@
 package com.example.miniinstagram.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.miniinstagram.R;
+import com.example.miniinstagram.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.protobuf.Value;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class MypageFragment extends Fragment {
+
+    private CircleImageView profileImageCircleImageView;
+    private ImageView optionsImageView;
+    private ImageView myPostsImageView;
+
+    private TextView postsTextView;
+    private TextView followersTextView;
+    private TextView followingTextView;
+    private TextView nameTextView;
+    private TextView bioTextView;
+    private TextView usernameTextView;
+
+    private FirebaseUser fbUser;
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
+
+    private String profileID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false);
+        View view = inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        storageReference = FirebaseStorage.getInstance().getReference("Posts_Image");
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        profileID = fbUser.getUid();
+
+        profileImageCircleImageView = view.findViewById((R.id.profile_image));
+        optionsImageView = view.findViewById(R.id.options);
+        myPostsImageView = view.findViewById(R.id.my_posts);
+
+        postsTextView = view.findViewById(R.id.posts);
+        followersTextView = view.findViewById(R.id.followers);
+        followingTextView = view.findViewById(R.id.following);
+        nameTextView = view.findViewById(R.id.name);
+        bioTextView = view.findViewById(R.id.bio);
+        usernameTextView = view.findViewById(R.id.username);
+
+        showUserBasicContent();
+
+
+        return view;
+    }
+
+    // Fetch and show basic content of user profile. such as info, number of followers and following, user posts, etc
+    private void showUserBasicContent() {
+        getUserInfo();
+    }
+
+    private void getUserInfo() {
+        DatabaseReference dbReference = databaseReference.child("Users").child(profileID);
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (getContext() == null) {
+                    return;
+                }
+
+                User user = snapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
     }
 }
