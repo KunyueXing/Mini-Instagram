197d1297d385d9cb486d2ea17040056928999b63
Kunyue Xing <scarlettyue1991@gmail.com>
2023-12-07 01:09:47 -0800

    Continued creating dynamic user lists with RecyclerView for Search fragment

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/Adapter/UserAdapter.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/Adapter/UserAdapter.java
@@ -1,5 +1,8 @@
 package com.example.miniinstagram.Adapter;
 
+import android.content.Context;
+import android.view.ContentInfo;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -10,29 +13,35 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.miniinstagram.R;
 import com.example.miniinstagram.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-//
+/**
+ * UserAdapter requests views, and binds the views to their data, by calling methods in the adapter.
+ * Define the adapter by extending RecyclerView.Adapter.
+ */
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private Context mContext;
+    private List<User> mUsers;
+    private boolean isFragment;
+    private FirebaseUser firebaseUser;
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    // We'll user UserAdapter both in fragment and activity, so we have a boolean param.
+    public UserAdapter(Context mContext, List<User> mUsers, boolean isFragment) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+        this.isFragment = isFragment;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    // Build ViewHolder and connect with user_item.xml
+    /**
+     * Each individual element in the list is defined by a view holder object.
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public CircleImageView profileImageCircleImageView;
@@ -49,4 +58,46 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             followButton = itemView.findViewById(R.id.follow_button);
         }
     }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item , parent , false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        final User user = mUsers.get(position);
+        holder.followButton.setVisibility(View.VISIBLE);
+        holder.usernameTextView.setText(user.getUsername());
+        holder.nameTextView.setText(user.getName());
+        Picasso.get()
+               .load(user.getProfilePicUriStr())
+               .placeholder(R.drawable.default_avatar)
+               .into(holder.profileImageCircleImageView);
+
+        isFollowed(user.getUserID(), holder.followButton);
+
+        // If user in search results is the current user herself, the follow button would disappear
+        if (user.getUserID().equals(firebaseUser.getUid())) {
+            holder.followButton.setVisibility(View.GONE);
+        }
+    }
+
+    // Return the size of user list, which is the search results (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    // To check if a user is followed by the current user and show corresponding results on button
+    private void isFollowed(final String userID, final Button button) {
+
+    }
+
+
 }
