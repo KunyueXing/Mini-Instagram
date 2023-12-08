ab11f4af2deae2966d2684aabf6b2a552d3f519d
Kunyue Xing <scarlettyue1991@gmail.com>
2023-12-07 18:17:29 -0800

    Start to add auto-completion to search fragment, with external package socialView.

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/SearchFragment.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/SearchFragment.java
@@ -3,19 +3,44 @@ package com.example.miniinstagram.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.miniinstagram.Adapter.UserAdapter;
 import com.example.miniinstagram.R;
+import com.example.miniinstagram.model.User;
+import com.hendraanggrian.appcompat.socialview.widget.SocialAutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<User> mUsers;
+    private UserAdapter userAdapter;
+    private SocialAutoCompleteTextView searchBarTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view_users);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(), mUsers, true);
+        recyclerView.setAdapter(userAdapter);
+
+        searchBarTextView = view.findViewById(R.id.search_bar);
+
+        return view;
     }
 }
