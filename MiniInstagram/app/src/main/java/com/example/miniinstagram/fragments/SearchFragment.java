967265eb3e413359fa49b4dbfd84fc607bf43e5e
Kunyue Xing <scarlettyue1991@gmail.com>
2023-12-07 22:18:15 -0800

    Finished search for users in Search fragment

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/SearchFragment.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/SearchFragment.java
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.ktx.Firebase;
 import com.hendraanggrian.appcompat.socialview.widget.SocialAutoCompleteTextView;
@@ -34,6 +37,7 @@ public class SearchFragment extends Fragment {
     private UserAdapter userAdapter;
     private SocialAutoCompleteTextView searchBarTextView;
     private String TAG = "Search fragment: ";
+    private DatabaseReference userDatabaseRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,7 +55,12 @@ public class SearchFragment extends Fragment {
         userAdapter = new UserAdapter(getContext(), mUsers, true);
         recyclerView.setAdapter(userAdapter);
 
+        userDatabaseRef = FirebaseDatabase.getInstance()
+                                          .getReference()
+                                          .child("Users");
+
         getAllUsers();
+        search();
 
         return view;
     }
@@ -60,9 +69,6 @@ public class SearchFragment extends Fragment {
      * When user doesn't type anything in the search bar, will show all users as default.
      */
     private void getAllUsers() {
-        DatabaseReference userDatabaseRef = FirebaseDatabase.getInstance()
-                                                            .getReference()
-                                                            .child("Users");
 
         ValueEventListener listener = new ValueEventListener() {
             @Override
@@ -85,6 +91,10 @@ public class SearchFragment extends Fragment {
                         mUsers.add(user);
                     }
 
+                    /**
+                     * Notifies the attached observers that the underlying data has been changed
+                     * and any View reflecting the data set should refresh itself.
+                     */
                     userAdapter.notifyDataSetChanged();
 
                     userDatabaseRef.removeEventListener(this);
@@ -99,5 +109,58 @@ public class SearchFragment extends Fragment {
         };
 
         userDatabaseRef.addValueEventListener(listener);
+    }
+
+    private void search() {
+        TextWatcher txtWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                searchUser(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+
+        searchBarTextView.addTextChangedListener(txtWatcher);
+    }
+
+    private void searchUser(String target) {
+        recyclerView.setVisibility(View.VISIBLE);
+
+        Query query = userDatabaseRef.orderByChild("username")
+                                     .startAt(target)
+                                     .endAt(target + "\uf8ff");
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUsers.clear();
+
+                for (DataSnapshot subSnapShot : snapshot.getChildren()) {
+                    User user = subSnapShot.getValue(User.class);
+                    mUsers.add(user);
+                }
+
+                userAdapter.notifyDataSetChanged();
+
+                query.removeEventListener(this);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed to read value when get searched users from Users in DB", error.toException());
+                query.removeEventListener(this);
+            }
+        };
+
+        query.addValueEventListener(listener);
     }
 }
