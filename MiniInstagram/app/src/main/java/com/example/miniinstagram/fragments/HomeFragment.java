1db1c8b0e51ddc3d7015c68aeef728c6af9c784a
Kunyue Xing <scarlettyue1991@gmail.com>
2023-12-23 13:06:46 -0800

    Get all users followed by the current user and store their userID in a list in HomeFragment

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/HomeFragment.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/HomeFragment.java
@@ -3,10 +3,12 @@ package com.example.miniinstagram.fragments;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,13 @@ import android.widget.ProgressBar;
 import com.example.miniinstagram.Adapter.PostAdapter;
 import com.example.miniinstagram.R;
 import com.example.miniinstagram.model.Post;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +32,13 @@ public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
     private PostAdapter postAdapter;
     private List<Post> postList;
-    private List<String> followingList;
+    private List<String> allFollowingList;
     private ProgressBar progressBar;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference databaseReference;
+    private String databaseFollowing = "User-following";
+    private String databasePosts = "Posts";
+    private String TAG = "HomeFragment: ";
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -32,6 +46,9 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         recyclerView = view.findViewById(R.id.recycler_view_posts);
         recyclerView.setHasFixedSize(true);
@@ -45,10 +62,43 @@ public class HomeFragment extends Fragment {
         postAdapter = new PostAdapter(getContext(), postList);
         recyclerView.setAdapter(postAdapter);
 
+        allFollowingList = new ArrayList<>();
+
         progressBar = view.findViewById(R.id.progress_circular);
 
-        
+        getAllFollowing();
+        getAllFollowingPosts();
 
         return view;
+    }
+
+    private void getAllFollowingPosts() {
+        
+    }
+
+    /**
+     * Get all users followed by the current user and store their userID in a list.
+     */
+    private void getAllFollowing() {
+        DatabaseReference ref = databaseReference.child(databaseFollowing)
+                                                 .child(firebaseUser.getUid());
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                allFollowingList.clear();
+
+                for (DataSnapshot subSnapshot : snapshot.getChildren()) {
+                    allFollowingList.add(subSnapshot.getKey());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.i(TAG, "Can't get all followings from User-following");
+            }
+        };
+
+        ref.addValueEventListener(listener);
     }
 }
