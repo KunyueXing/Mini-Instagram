0ad5ec5e7dc99f63fd7f1ec84036a341aaa25389
Kunyue Xing <scarlettyue1991@gmail.com>
2024-01-05 13:49:43 -0800

    Show user profile image when comment

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/UI_Activity/CommentActivity.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/UI_Activity/CommentActivity.java
@@ -1,17 +1,26 @@
 package com.example.miniinstagram.UI_Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.miniinstagram.R;
+import com.example.miniinstagram.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,9 +31,14 @@ public class CommentActivity extends AppCompatActivity {
     private TextView postTextView;
 
     private FirebaseUser fbUser;
+    private DatabaseReference databaseReference;
 
     private String postID;
     private String authorID;
+    private String TAG = "CommentActivity: ";
+    private String databaseUsers = "Users";
+    private String databasePostComments = "Post-comments";
+    private String databaseComments = "Comments";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,5 +70,38 @@ public class CommentActivity extends AppCompatActivity {
         postTextView = findViewById(R.id.post);
 
         fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        getUserProfileImage();
+    }
+
+    /**
+     * Show user profile image
+     */
+    private void getUserProfileImage() {
+        DatabaseReference ref = databaseReference.child(databaseUsers).child(fbUser.getUid());
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+
+                if (user.getProfilePicUriStr().equals("default")) {
+                    profileImageImageView.setImageResource(R.drawable.default_avatar);
+                } else {
+                    Picasso.get()
+                           .load(user.getProfilePicUriStr())
+                           .placeholder(R.drawable.default_avatar)
+                           .into(profileImageImageView);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "onCancelled: Can't access User profile", error.toException());
+            }
+        };
+
+        ref.addListenerForSingleValueEvent(listener);
     }
 }
