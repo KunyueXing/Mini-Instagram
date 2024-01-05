b52361978a717a95249de850c22b99972353c075
Kunyue Xing <scarlettyue1991@gmail.com>
2024-01-04 23:56:37 -0800

    Finished interface of comment activity.

---
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/UI_Activity/CommentActivity.java
@@ -0,0 +1,16 @@
+package com.example.miniinstagram.UI_Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.miniinstagram.R;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
