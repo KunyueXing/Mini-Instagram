dfe0bfad51a0bdd0a2aef8bb83d2f5651f9b052e
Kunyue Xing <scarlettyue1991@gmail.com>
2024-01-19 01:48:25 -0800

    Initialized dynamic list of items in Notification fragment and show all notifications of the current user.

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/NotificationFragment.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/fragments/NotificationFragment.java
@@ -2,20 +2,86 @@ package com.example.miniinstagram.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.miniinstagram.Adapter.NotificationAdapter;
 import com.example.miniinstagram.R;
+import com.example.miniinstagram.model.Notification;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class NotificationFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private NotificationAdapter notificationAdapter;
+    private List<Notification> notificationList;
+    private String databaseNotifications = "Notifications";
+    private String TAG = "Notification Fragment: ";
+    private FirebaseUser fbUser;
+    private DatabaseReference databaseRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        notificationList = new ArrayList<>();
+        notificationAdapter = new NotificationAdapter(getContext(), notificationList);
+        recyclerView.setAdapter(notificationAdapter);
+
+        fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+
+        getNotifications();
+
+        return view;
+    }
+
+    /**
+     * Get all notifications of the current user from the database and save in the list
+     */
+    private void getNotifications() {
+        DatabaseReference ref = databaseRef.child(databaseNotifications).child(fbUser.getUid());
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                notificationList.clear();
+                for (DataSnapshot subSnapshot : snapshot.getChildren()) {
+                    Notification notification = subSnapshot.getValue(Notification.class);
+                    notificationList.add(notification);
+                }
+
+                Collections.reverse(notificationList);
+                notificationAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "onCancelled: Failed to get the notifications", error.toException());
+            }
+        };
+
+        ref.addListenerForSingleValueEvent(listener);
     }
 }
