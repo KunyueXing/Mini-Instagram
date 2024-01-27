48233f5ef3abe1f7ba37972ab60e97c822c28619
Kunyue Xing <scarlettyue1991@gmail.com>
2024-01-27 04:38:23 -0800

    Added show group members in group detail page.

--- MiniInstagram/app/src/main/java/com/example/miniinstagram/UI_Activity/GroupDetailActivity.java
+++ MiniInstagram/app/src/main/java/com/example/miniinstagram/UI_Activity/GroupDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.miniinstagram.UI_Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -12,9 +13,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.miniinstagram.Adapter.GroupAdapter;
+import com.example.miniinstagram.Adapter.UserAdapter;
 import com.example.miniinstagram.R;
 import com.example.miniinstagram.model.Group;
+import com.example.miniinstagram.model.GroupAdapterCode;
 import com.example.miniinstagram.model.User;
+import com.example.miniinstagram.model.UserAdapterCode;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,7 +28,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupDetailActivity extends AppCompatActivity {
     public TextView groupNameTextView;
@@ -32,6 +40,7 @@ public class GroupDetailActivity extends AppCompatActivity {
     public TextView descriptionTextView;
     public RecyclerView recyclerView;
     public ImageView closeImageView;
+    private UserAdapter userAdapter;
 
     private List<User> mUsers;
     private String groupID;
@@ -55,6 +64,13 @@ public class GroupDetailActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_view);
         closeImageView = findViewById(R.id.closeImageView);
 
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManagerGroup = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManagerGroup);
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(this, mUsers, UserAdapterCode.USER_ADAPTER_CODE_GENERAL);
+        recyclerView.setAdapter(userAdapter);
+
         fbUser = FirebaseAuth.getInstance().getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
@@ -62,6 +78,64 @@ public class GroupDetailActivity extends AppCompatActivity {
         groupID = intent.getStringExtra("groupID");
 
         getGroupInfo();
+        getUserList();
+    }
+
+    private void getUserList() {
+        Set<String> userIDList  = getUserIDs();
+        if (userIDList == null) {
+            return;
+        }
+        DatabaseReference ref = databaseReference.child(databaseUsers);
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUsers.clear();
+
+                for (DataSnapshot subSnapshot : snapshot.getChildren()) {
+                    User user = subSnapshot.getValue(User.class);
+
+                    assert user != null;
+                    if (userIDList.contains(user.getUserID())) {
+                        mUsers.add(user);
+                    }
+                }
+
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed get users from User database", error.toException());
+            }
+        };
+
+        ref.addListenerForSingleValueEvent(listener);
+    }
+
+    private Set<String> getUserIDs() {
+        Set<String> result = new HashSet<>();
+
+        DatabaseReference ref = databaseReference.child(databaseGroups).child(groupID);
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Group group = snapshot.getValue(Group.class);
+                for (String userID : group.getMembers().keySet()) {
+                    result.add(userID);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed get user ids from the group", error.toException());
+            }
+        };
+
+        ref.addListenerForSingleValueEvent(listener);
+        return result;
     }
 
     /**
